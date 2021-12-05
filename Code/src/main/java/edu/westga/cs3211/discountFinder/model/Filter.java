353e6cf498cb355abc5b0c1098b35df0467eaa11
Dowee Maynard <Dowee24@gmail.com>
2021-12-05 15:07:02 -0500

    Added Filter Functionality and Filter Test

---
+++ Code/src/main/java/edu/westga/cs3211/discountFinder/model/Filter.java
@@ -0,0 +1,68 @@
+package edu.westga.cs3211.discountFinder.model;
+
+import java.util.ArrayList;
+
+public class Filter {
+    
+    public static ArrayList<Item> filterByCategory(ArrayList<Item> list, Category category) {
+        ArrayList<Item> filteredList = new ArrayList<>();
+        for (Item item : list) {
+            if (item.getCategory() == category) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
+    public static ArrayList<Item> filterBySeller(ArrayList<Item> list, String seller) {
+        ArrayList<Item> filteredList = new ArrayList<>();
+        for (Item item : list) {
+            if (item.getSeller().getSellerName().toLowerCase().contains(seller.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
+    public static ArrayList<Item> filterByName(ArrayList<Item> list, String name) {
+        ArrayList<Item> filteredList = new ArrayList<>();
+        for (Item item : list) {
+            if (item.getName().toLowerCase().startsWith(name.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
+    public static ArrayList<Item> filterByDistance(ArrayList<Item> list, DistanceEnum distanceto) {
+        ArrayList<Item> filteredList = new ArrayList<>();
+        for (Item item : list) {
+
+            switch (distanceto) {
+                case Ten:
+                    if (item.getSeller().getDistance() <= 10) {
+                        filteredList.add(item);
+                    }
+                    break;
+                
+                case Twenty:
+                    if (item.getSeller().getDistance() <= 20) {
+                        filteredList.add(item);
+                    }
+                    break;
+                
+                case Thirty:
+                    if (item.getSeller().getDistance() <= 30) {
+                        filteredList.add(item);
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        return filteredList;
+    }
+
+
+}
---
+++ Code/src/test/java/edu/westga/cs3211/discountFinder/test/model/discountFinder/TestFilter/TestFilterBySeller.java
@@ -0,0 +1,46 @@
+package edu.westga.cs3211.discountFinder.test.model.discountFinder.TestFilter;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3211.discountFinder.model.DiscountFinder;
+import edu.westga.cs3211.discountFinder.model.Filter;
+import edu.westga.cs3211.discountFinder.model.Item;
+
+public class TestFilterBySeller {
+    
+    @Test
+    void TestNoItemMatches() {
+        DiscountFinder finder = new DiscountFinder();
+
+        ArrayList<Item> filtered = Filter.filterBySeller(finder.getItems(), "publix");
+
+        assertEquals(filtered.size(), 0);
+
+    }
+
+    @Test
+    void TestOneItemMatches() {
+        DiscountFinder finder = new DiscountFinder();
+
+        ArrayList<Item> filtered = Filter.filterBySeller(finder.getItems(), "Adam");
+
+        assertEquals(filtered.size(), 1);
+        assertEquals(filtered.get(0), finder.getItem(7));
+    }
+
+    @Test
+    void TestTwoItemMatches() {
+        DiscountFinder finder = new DiscountFinder();
+
+        ArrayList<Item> filtered = Filter.filterBySeller(finder.getItems(), "Del");
+
+        assertEquals(filtered.size(), 2);
+        assertEquals(filtered.get(0), finder.getItem(0));
+        assertEquals(filtered.get(1), finder.getItem(1));
+    }
+}
