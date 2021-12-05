c40b85fddb27c566cb773e8f6042591d1aad9381
Trevor Johnson <tjohns52@my.westga.edu>
2021-12-05 15:52:46 -0500

    Fixed minor warnings and errors

--- Code/src/test/java/edu/westga/cs3211/discountFinder/test/model/discountFinder/TestFilter/TestFilterBySeller.java
+++ Code/src/test/java/edu/westga/cs3211/discountFinder/test/model/discountFinder/TestFilter/TestFilterBySeller.java
@@ -1,6 +1,6 @@
 package edu.westga.cs3211.discountFinder.test.model.discountFinder.TestFilter;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
